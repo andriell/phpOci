74cfed6430c68f1b5630dcb211750d8a0d540c3f
rybalko <rybalko@spcom.biz>
2015-02-13 17:09:54 +0300

    Data

--- class/orm/CamelCase.php
+++ class/orm/CamelCase.php
@@ -6,25 +6,42 @@
  */
 
 class CamelCase {
+    private static $lCC = array();
+    private static $uCC = array();
+    private static $lUS = array();
+    private static $uUS = array();
+
     public static function lCC($str) {
-        $str = strtolower($str);
-        return preg_replace_callback('#(_+(\S))#si', function ($m) {
-            return strtoupper($m[2]);
-        }, $str);
+        if (!isset(self::$lCC[$str])) {
+            $str = strtolower($str);
+            self::$lCC[$str] = preg_replace_callback('#(_+(\S))#si', function ($m) {
+                return strtoupper($m[2]);
+            }, $str);
+        }
+        return self::$lCC[$str];
     }
 
     public static function uCC($str) {
-        $str = self::lCC($str);
-        return strtoupper(substr($str, 0, 1)) . substr($str, 1);
+        if (!isset(self::$uCC[$str])) {
+            $str = self::lCC($str);
+            self::$uCC[$str] = strtoupper(substr($str, 0, 1)) . substr($str, 1);
+        }
+        return self::$uCC[$str];
     }
 
     public static function lUS($str) {
-        $str = preg_replace('#_{2,}#', '_', $str);
-        return strtolower($str);
+        if (!isset(self::$lUS[$str])) {
+            $str = preg_replace('#_{2,}#', '_', $str);
+            self::$lUS[$str] = strtolower($str);
+        }
+        return self::$lUS[$str];
     }
 
     public static function uUS($str) {
-        $str = preg_replace('#_{2,}#', '_', $str);
-        return strtoupper($str);
+        if (!isset(self::$uUS[$str])) {
+            $str = preg_replace('#_{2,}#', '_', $str);
+            self::$uUS[$str] = strtoupper($str);
+        }
+        return self::$uUS[$str];
     }
 } 
